98314665ed74be6d379b8d34f2fb8eddf5006864
Corinne <corinne.leutert@stud.hslu.ch>
2024-12-27 19:58:36 +0100

    Update STL_zusammenführen.py
    Definition daraus erstellt  um besser im Hauptskript zu integrieren

--- Funktionieren/STL_zusammenführen.py
+++ Funktionieren/STL_zusammenführen.py
@@ -1,14 +1,26 @@
 from stl import mesh
 import numpy as np
 
-# Laden der STL-Dateien
-mesh1 = mesh.Mesh.from_file("solid.stl")
-mesh2 = mesh.Mesh.from_file("dach.stl")
-mesh3 = mesh.Mesh.from_file("gelände.stl")
+def combine_stl_files(stl_output_file, *input_files):
+    """
+    Kombiniert mehrere STL-Dateien zu einer einzigen und speichert sie.
 
-# Kombiniere der Meshes
-combined_mesh = mesh.Mesh(np.concatenate([mesh1.data, mesh2.data, mesh3.data]))
+    Parameters:
+    output_file (str): Der Name der Ausgabedatei.
+    input_files (str): Die Namen der Eingabedateien.
 
-# Exportiere das kombinierte Mesh in eine neue STL-Datei
-combined_mesh.save("kombiniert.stl")
-print("STL-Dateien erfolgreich zusammengeführt und exportiert!")
+    Returns:
+    None
+    """
+    # Laden der STL-Dateien
+    meshes = [mesh.Mesh.from_file(file) for file in input_files]
+
+    # Kombinieren der Meshes
+    combined_mesh = mesh.Mesh(np.concatenate([m.data for m in meshes]))
+
+    # Exportieren des kombinierten Mesh in eine neue STL-Datei
+    combined_mesh.save(stl_output_file)
+    print(f"STL-Dateien erfolgreich zusammengeführt und in '{stl_output_file}' exportiert!")
+
+# Beispielaufruf der Funktion
+combine_stl_files("kombiniert.stl", "solid.stl", "dach.stl")
