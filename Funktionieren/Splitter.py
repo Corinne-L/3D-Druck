41eb86f15929d7e4ca5cb56b20c415c3a0ce071a
Corinne <corinne.leutert@stud.hslu.ch>
2024-12-28 16:50:57 +0100

    Update Splitter.py
    Ergänzung von Typehints und Dogstrings

--- Funktionieren/Splitter.py
+++ Funktionieren/Splitter.py
@@ -3,15 +3,26 @@ import tempfile
 import os
 import streamlit as st
 
-# Funktion zum Filtern und Entfernen von Entitäten basierend auf Layern
-def split_and_remove_entities(dxf_split, layers_to_remove):
+# DXF Spliten nach Layer
+def split_and_remove_entities(dxf_split, layers_to_remove) -> tuple:
+    """
+    Liest eine DXF-Datei ein, entfernt Entitäten, die auf den angegebenen Layern basieren,
+    und gibt das bearbeitete DXF-Dokument sowie die entfernten Entitäten zurück.
+
+    Parameters:
+    dxf_split (io.BytesIO): Die hochgeladene DXF-Datei als BytesIO-Objekt.
+    layers_to_remove (List[str]): Eine Liste der Layer, von denen die Entitäten entfernt werden sollen.
+
+    Returns:
+    Das bearbeitete DXF-Dokument.
+    """
     # Überprüfe, ob dxf_split eine Datei ist und Inhalt enthält
     if dxf_split is None:
         st.error("Die DXF-Datei wurde nicht hochgeladen oder ist leer.")
         st.stop()  # Stoppt die Ausführung
 
     try:
-        # Temporäre Datei erstellen
+        # Temporäre Datei erstellen (Streamlit)
         with tempfile.NamedTemporaryFile(delete=False, suffix=".dxf") as temp_dxf:
             temp_dxf.write(dxf_split.read())  # Schreiben des Inhalts in die temporäre Datei
             temp_dxf_path = temp_dxf.name  # Pfad zur temporären Datei
@@ -41,8 +52,18 @@ def split_and_remove_entities(dxf_split, layers_to_remove):
     os.remove(temp_dxf_path)  # Temporäre Datei löschen
     return doc, entities_to_export
 
-def save_dxf(doc, output_file):
-    # DXF speichern
+# DXF speichern
+def save_dxf(doc: ezdxf.document.DXFDocument, output_file: str) -> None:
+    """
+    Speichert das DXF-Dokument in einer Datei.
+
+    Parameter:
+    doc (ezdxf.document.DXFDocument): Das DXF-Dokument, das gespeichert werden soll.
+    output_file (str): Der Pfad, unter dem die Datei gespeichert wird.
+
+    Returns:
+    None: Gibt keine Rückgabewerte zurück, da die Datei gespeichert wird.
+    """
     doc.saveas(output_file)
     print(f"Datei gespeichert: {output_file}")
 
