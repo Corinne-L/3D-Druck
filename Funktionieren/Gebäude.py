6e8504261d2c7e0bc6694da46123482c8fdbd8c6
Corinne <corinne.leutert@stud.hslu.ch>
2024-12-29 09:43:36 +0100

    Update Gebäude.py

--- Funktionieren/Gebäude.py
+++ Funktionieren/Gebäude.py
@@ -1,46 +1,49 @@
 import win32com.client
+import streamlit as st
 
-# AutoCAD-Instanz starten oder verbinden
-acad = win32com.client.Dispatch("AutoCAD.Application")
-acad.Visible = True  # AutoCAD sichtbar machen
 
-# DXF-Datei öffnen
-filename = r"C:\Users\Corinne\OneDrive - Hochschule Luzern\3. Semester_HS24\DC_Programming\3D Druck\solid.dxf"
-doc = acad.Documents.Open(filename)
 
 def convert(doc):
     try:
         doc.SendCommand(f"_MESHSMOOTH\n_all\n\n")
-        print("Befehl erfolgreich gesendet!")
     except Exception as e:
-        print(f"Fehler beim Verarbeiten der Datei: {e}")
+        st.warning(f"Fehler beim Verarbeiten der Datei: {e}")
 
-def export(doc):
+def export(doc, output_stl_g):
     try:
         # Export as STL
-        stl_filename = r"C:\Users\Corinne\OneDrive - Hochschule Luzern\3. Semester_HS24\DC_Programming\3D Druck\solid.stl"
+        stl_filename = output_stl_g
         doc.SendCommand(f'_STLOUT\n_all\n\n\n{stl_filename}\n\n')
-        print("Befehl erfolgreich gesendet!")
     except Exception as e:
-        print(f"Fehler beim Verarbeiten der Datei: {e}")
+        st.warning(f"Fehler beim Verarbeiten der Datei: {e}")
 
 def save_all(doc): 
     try: 
-        doc.SendCommand("_QSAVE\n\n\n\n\nJ\n") 
-        print("Alle Dateien erfolgreich gespeichert!") 
+        doc.SendCommand("_QSAVE\n\n\n\n\n") 
     except Exception as e: 
-        print(f"Fehler beim Speichern der Dateien: {e}")
+        st.warning(f"Fehler beim Speichern der Dateien: {e}")
 
 def close_acad(acad): 
     try: 
         acad.Quit() 
-        print("AutoCAD erfolgreich geschlossen!") 
+        st.success("AutoCAD erfolgreich geschlossen!") 
     except Exception as e: 
-        print(f"Fehler beim Schließen von AutoCAD: {e}")
+        st.warning(f"Fehler beim Schließen von AutoCAD: {e}")
 
-# Funktion aufrufen
-convert(doc)
-export(doc)
-save_all(doc)
-close_acad(acad)
 
+
+## AutoCAD-Instanz starten oder verbinden
+#acad = win32com.client.Dispatch("AutoCAD.Application")
+#acad.Visible = True  # AutoCAD sichtbar machen
+
+# DXF-Datei öffnen
+#filename = r"C:\Users\Corinne\OneDrive - Hochschule Luzern\3. Semester_HS24\DC_Programming\3D Druck\solid.dxf"
+#doc = acad.Documents.Open(filename) 
+
+
+
+ ## Funktion aufrufen
+#convert(doc)
+#export(doc)
+#save_all(doc)
+#close_acad(acad) 
